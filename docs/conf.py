3b8488eb326edd5ae8a9e8de451566d86f068773
Zixuan Wang <96053899+wdk0082@users.noreply.github.com>
2025-11-04 22:17:00 +0000

    Documentation revised

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'autodoc'
+copyright = '2025, wdk0082'
+author = 'wdk0082'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'collapse_navigation': False,
+    'navigation_depth': 4,
+    'sticky_navigation': True,
+}
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. autodoc documentation master file, created by
+   sphinx-quickstart on Tue Nov  4 19:15:03 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+autodoc documentation
+=====================
+
+This documentation provides an overview of the project along with an automatically generated API reference using Sphinx autodoc.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+API Reference
+=============
+
+.. automodule:: autodoc.base_people
+   :members:
+   :undoc-members:
+   :show-inheritance:
